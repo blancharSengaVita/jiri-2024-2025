68355f888af7badfa02cebc8a48f215318958c54
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2024-09-23 17:18:02 +0200

    Add test for other validation of login

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -37,22 +37,30 @@ $login = function () {
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
                         address</label>
                     <div class="mt-2">
-                        <input id="email" name="email" wire:model="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        <input dusk="email" id="email" name="email" wire:model="form.email"
+                               type="email"
+                               autocomplete="email"
+                               required
+                               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                     <x-input-error dusk="email-error" :messages="$errors->get('form.email')" class="mt-2"/>
                 </div>
 
                 <div>
-                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <label dusk="password" for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <div class="mt-2">
-                        <input id="password" name="password" wire:model="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focs:ring-indigo-600 sm:text-sm sm:leading-6">
+                        <input dusk="password" id="password" name="password" wire:model="form.password"
+                               type="password"
+                               autocomplete="current-password"
+                               required
+                               class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focs:ring-indigo-600 sm:text-sm sm:leading-6">
                     </div>
                     <x-input-error dusk="password-error" :messages="$errors->get('form.password')" class="mt-2"/>
                 </div>
 
                 <div class="flex items-center justify-between">
                     <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" wire:model="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                        <input id="remember-me" name="remember-me" wire:model="form.remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
                         <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-900">Remember
                             me</label>
                     </div>
--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -1,29 +1,72 @@
 <?php
 
+use Illuminate\Support\Facades\Lang;
 use Laravel\Dusk\Browser;
 use App\Models\User;
 
-test('example', function () {
+
+test('that shows front-end errors of login form', function () {
     $this->browse(function (Browser $browser) {
-        $browser->visit('/')
-            ->assertSee('Laravel');
+        $browser->visit('/login')
+            ->assertAttribute('@email', 'type', 'email')
+            ->assertAttribute('@email', 'required', '')
+            ->assertAttribute('@password', 'type', 'password')
+            ->assertAttribute('@password', 'required', '');
     });
 });
 
+//désactiver le required des inputs pour ce test ou alors demander au prof
+//test('that shows errors of login form', function () {
+//    $this->browse(function (Browser $browser) {
+//        $browser->visit('/login')
+//            ->press('Sign in')
+//            ->waitFor('@email-error')
+//            ->assertPresent('@email-error')
+//            ->waitFor('@password-error')
+//            ->assertPresent('@password-error');
+//    });
+//});
 
-test('shows errors of login form', function () {
-
+test('that there is no required error when email and password are populate', function () {
     $this->browse(function (Browser $browser) {
         $browser->visit('/login')
             ->type('email', 'bla@bla.com')
             ->type('password', 'password')
             ->press('Sign in')
             ->waitFor('@email-error')
-            ->assertPresent('@email-error')
-            ->waitFor('@password-error')
-            ->assertPresent('@password-error');
+            ->assertDontSeeIn('@email-error', __('validation.required', ['attribute' => 'email']))
+            ->assertMissing('@password-error');
     });
 });
+
+test('that it has a french validation message', function(){
+        $this->assertTrue(
+        Lang::hasForLocale('validation.required', 'en'),
+        'English validation message not found'
+    );
+});
+
+
+//        public function it_has_an_english_validation_message()
+//{
+//    $this->assertTrue(
+//        \Lang::hasForLocale('validation.is_tall', 'en'),
+//        'English validation message not found'
+//    );
+//}
+
+//test('that a user can login', function () {
+//    $this->browse(function (Browser $browser) {
+//        $browser->visit('/login')
+//            ->type('email', 'test@example.com')
+//            ->type('password', 'password')
+//            ->press('Sign in')
+//            ->assertPathIs('/dashboard');
+//    });
+//});
+
+
+
 
 
 
