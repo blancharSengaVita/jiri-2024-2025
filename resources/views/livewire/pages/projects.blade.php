2c885f42f9267f5ccd0fde232d79b3ff746634e8
Blanchar Senga-Vita <blanchar.sengavita@student.hepl.be>
2024-11-08 12:13:27 +0100

    Add dynamique input

--- resources/views/livewire/pages/projects.blade.php
+++ resources/views/livewire/pages/projects.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Project;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Masmerise\Toaster\Toaster;
 use function Livewire\Volt\{layout, mount, rules, state};
@@ -14,6 +15,8 @@ state([
     'user',
     'name',
     'description',
+    'linkInputs',
+    'tasks',
     'id',
     'deleteModal',
 ]);
@@ -21,9 +24,13 @@ state([
 rules(fn() => [
     'name' => 'required',
     'description' => 'required',
+    'linkInputs.*' => 'required',
+    'tasks.*' => 'required',
 ])->messages([
     'name.required' => 'Le champ est obligatoire.',
     'description.required' => 'Le champ est obligatoire.',
+    'linkInputs.*.required' => 'Le champ est obligatoire.',
+    'tasks.*.required' => 'Le champ est obligatoire.',
 ])->attributes([
 ]);
 
@@ -35,11 +42,31 @@ mount(function () {
     $this->id = 0;
     $this->name = '';
     $this->description = '';
+    $this->linkInputs = new Collection();
+    $this->tasks = new Collection();
 });
+
+$addLinkInput = function () {
+    $this->linkInputs->push('');
+};
+
+$removeLinkInput = function ($key) {
+    $this->linkInputs->pull($key);
+};
+
+$addTasks = function () {
+    $this->tasks->push('');
+};
+
+$removeTasks = function ($key) {
+    $this->tasks->pull($key);
+};
 
 $openCreateDrawer = function () {
     $this->name = '';
     $this->description = '';
+    $this->linkInputs = new Collection();
+    $this->tasks = new Collection();
     $this->drawer = true;
 };
 
@@ -50,6 +77,8 @@ $closeCreateDrawer = function () {
 $edit = function (Project $project) {
     $this->name = $project->name;
     $this->description = $project->description;
+    $this->linkInputs = collect(json_decode($project->links));
+    $this->tasks = collect(json_decode($project->tasks));
     $this->drawer = true;
 };
 
@@ -67,8 +96,9 @@ $save = function () {
         [
             'name' => $this->name,
             'description' => $this->description,
+            'links' => json_encode($this->linkInputs),
+            'tasks' => json_encode($this->tasks),
         ]);
-
 
     $this->drawer = false;
     if ($this->id === 0) {
@@ -81,8 +111,6 @@ $save = function () {
 
     $this->mount();
 };
-
-
 
 $delete = function () {
     $this->project->delete();
@@ -157,7 +185,7 @@ $closeDeleteModal = function () {
 
         <div class="fixed inset-0 overflow-hidden">
             <div class="absolute inset-0 overflow-hidden">
-                <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+                <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full w-auto pl-10 sm:pl-16">
                     <!--
                       Slide-over panel, show/hide based on slide-over state.
 
@@ -169,12 +197,13 @@ $closeDeleteModal = function () {
                         To: "translate-x-full"
                     -->
                     <div class="pointer-events-auto w-screen max-w-md">
-                        <form wire:submit.prevent="save"
-                              @click.away="open = false "
-                              enctype="multipart/form-data"
-                              class="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl"
+                        <form
+                            wire:submit.prevent="save"
+                            @click.away="open = false "
+                            enctype="multipart/form-data"
+                            class="flex h-full flex-col divide-y divide-gray-200 bg-white shadow-xl"
                         >
-                            <div class="h-0 flex-1 overflow-y-auto">
+                            <div class="h-0 flex-1 overflow-y-scroll overflow-hidden max-h-full">
                                 <div class="bg-gray-900 px-4 py-6 sm:px-6">
                                     <div class="flex items-center justify-between">
                                         <h2 class="text-base font-semibold leading-6 text-white" id="slide-over-title">
@@ -214,7 +243,6 @@ $closeDeleteModal = function () {
                                                     @endif
                                                 </div>
                                             </div>
-
                                             <div>
                                                 <label for="description"
                                                        class="block text-sm font-medium leading-6 text-gray-900">Description</label>
@@ -233,11 +261,57 @@ $closeDeleteModal = function () {
                                                 @endif
                                             </div>
 
+
+                                            <div>
+                                                {{ $linkInputs }}
+                                                <label for="addLinks"
+                                                       class="text-sm font-medium leading-6 text-gray-900 flex gap-1 ">Le
+                                                    nom
+                                                    des liens qui seront attribués aux projets
+                                                    <button class="display-none create-project__input" id="addLinks"
+                                                            type="button"
+                                                            value="le nom des liens qui seront attribués aux projets"
+                                                            wire:click="addLinkInput">
+                                                        <svg class="create-project__svg addLink button__icon"
+                                                             xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                             viewBox="0 0 16 16">
+                                                            <path fill="currentColor" d="M14 7v1H8v6H7V8H1V7h6V1h1v6z"/>
+                                                        </svg>
+                                                    </button>
+                                                </label>
+                                                <div class="mt-2">
+                                                    <ul>
+
+                                                    </ul>
+                                                    @foreach($linkInputs as $key => $input)
+                                                        <div>
+                                                            <input wire:model.live="linkInputs.{{$key}}" type="text"
+                                                                   name="addLinks"
+                                                                   id="addLinks"
+                                                                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                                   autocomplete="on">
+                                                            <svg class=""
+                                                                 wire:click="removeLinkInput({{$key}})"
+                                                                 xmlns="http://www.w3.org/2000/svg" width="16"
+                                                                 height="16" viewBox="0 0 16 16">
+                                                                <path fill="currentColor" fill-rule="evenodd"
+                                                                      d="m8 8.707l3.646 3.647l.708-.707L8.707 8l3.647-3.646l-.707-.708L8 7.293L4.354 3.646l-.707.708L7.293 8l-3.646 3.646l.707.708z"
+                                                                      clip-rule="evenodd"/>
+                                                            </svg>
+                                                        </div>
+                                                        @if ($messages = $errors->get('linkInputs.' .$key))
+                                                            <div class="text-sm text-red-600 space-y-1 mt-2">
+                                                                <p>{{$messages[0]}}</p>
+                                                            </div>
+                                                        @endif
+                                                    @endforeach
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex flex-shrink-0 justify-end px-4 py-4">
+                            <div class="flex flex-shrink-0 justify-end px-4 py-4 bg-white">
                                 <button type="button"
                                         wire:click="closeCreateDrawer"
                                         class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -94,23 +94,31 @@ $openMobileMenu = function () {
                                 <ul role="list" class="-mx-2 space-y-1">
                                     <li>
                                         <!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
-                                        <a href="#" class="group flex gap-x-3 rounded-md bg-gray-800 p-2 text-sm font-semibold leading-6 text-white">
+                                        <a wire:navigate
+                                           href="{{route('pages.dashboard')}}"
+                                           class=" {{ Route::is('pages.dashboard') ? 'bg-gray-800 p-2 text-sm font-semibold leading-6 text-white' : 'p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white' }} group flex gap-x-3 rounded-md"
+                                        >
                                             <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                                             </svg>
                                             Dashboard
                                         </a>
                                     </li>
+                                    {{--                            <li>--}}
+                                    {{--                                <a
+                                    wire:navigate
+                                    href="{{route('pages.jiris')}}"--}}
+                                    {{--                                   class=" {{ Route::is('pages.jiris') ? 'bg-gray-800 p-2 text-sm font-semibold leading-6 text-white' : 'p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white' }} group flex gap-x-3 rounded-md">--}}
+                                    {{--                                    <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">--}}
+                                    {{--                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />--}}
+                                    {{--                                    </svg>--}}
+                                    {{--                                    Jiris--}}
+                                    {{--                                </a>--}}
+                                    {{--                            </li>--}}
                                     <li>
-                                        <a href="#" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                            <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-                                            </svg>
-                                            Jiries
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
+                                        <a wire:navigate
+                                            href="{{route('pages.projects')}}"
+                                           class=" {{ Route::is('pages.projects') ? 'bg-gray-800 p-2 text-sm font-semibold leading-6 text-white' : 'p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white' }} group flex gap-x-3 rounded-md">
                                             <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                                             </svg>
@@ -118,7 +126,9 @@ $openMobileMenu = function () {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="#" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
+                                        <a wire:navigate
+                                           href="{{route('pages.contacts')}}"
+                                           class="{{ Route::is('pages.contacts') ? 'bg-gray-800 p-2 text-sm font-semibold leading-6 text-white' : 'p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white' }} group flex gap-x-3 rounded-md">
                                             <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                                             </svg>
@@ -157,16 +167,20 @@ $openMobileMenu = function () {
                                     Dashboard
                                 </a>
                             </li>
+{{--                            <li>--}}
+{{--                                <a href="{{route('pages.jiris')}}"--}}
+{{--                                   class=" {{ Route::is('pages.jiris') ? 'bg-gray-800 p-2 text-sm font-semibold leading-6 text-white' : 'p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white' }} group flex gap-x-3 rounded-md">--}}
+{{--                                    <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">--}}
+{{--                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />--}}
+{{--                                    </svg>--}}
+{{--                                    Jiris--}}
+{{--                                </a>--}}
+{{--                            </li>--}}
                             <li>
-                                <a href="#" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
-                                    <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-                                    </svg>
-                                    Jiries
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white">
+                                <a
+                                    wire:navigate
+                                    href="{{route('pages.projects')}}"
+                                   class=" {{ Route::is('pages.projects') ? 'bg-gray-800 p-2 text-sm font-semibold leading-6 text-white' : 'p-2 text-sm font-semibold leading-6 text-gray-400 hover:bg-gray-800 hover:text-white' }} group flex gap-x-3 rounded-md">
                                     <svg class="h-6 w-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
                                     </svg>
