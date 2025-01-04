960136b17e744a74a26e7e0cf9d8f253b570f1b4
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2025-01-04 18:34:26 +0100

    Update disconnect button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,6 @@
 {{--                <livewire:layout.navigation />--}}
     <!-- Page Heading -->
     @if (isset($header))
-    @dump($header)
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 {{ $header }}
